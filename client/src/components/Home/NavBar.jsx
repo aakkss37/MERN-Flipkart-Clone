c62592ae0d77de4b8b349faa23ca29cc213f4d28
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-01 00:03:15 +0530

    navbar styling completed

--- client/src/components/Home/NavBar.jsx
+++ client/src/components/Home/NavBar.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
-import { styled, Box } from '@mui/material'
+import { styled, Box, Typography } from '@mui/material'
 import { navData } from '../../constant/data'
 
 
 
 const Component = styled(Box)`
 	display: flex;
-	margin: 60px 140px 0 140px;
+	margin: 55px 140px 0 140px;
 	justify-content: space-between;
 `
 const Conatiner = styled(Box)`
 	padding: 12px 8px;
+	text-align: center;
+`
+const Text = styled(Typography)`
+	font-size: 14px;
+	font-weight: 600;
+	font-family: inharit;
 `
 
 
@@ -22,7 +28,7 @@ const NavBar = () => {
 				navData.map(data=>(
 					<Conatiner>
 						<img src={data.url} alt='img' style={{width: 64}}/>
-						<p>{data.text}</p>
+						<Text>{data.text}</Text>
 					</Conatiner>
 				))
 			}
