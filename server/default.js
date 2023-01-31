484238b8533f2dbeb79a88da1761d39f161b57d2
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-01 03:41:19 +0530

    push dummy data to the databas

--- server/default.js
+++ server/default.js
@@ -1,9 +1,12 @@
 import { products } from "./constant/data";
+import Products from "./modal/productSchema";
 
-const DefaultData = ()=>{
+const DefaultData = async() => {
 	try {
-		
+		const responce = await Products.insertMany(products);
+		console.log("Data imported sucessfully");
 	} catch (error) {
 		console.log("Error while incerting default data: ", error.message)
 	}
 }
+export default DefaultData;
