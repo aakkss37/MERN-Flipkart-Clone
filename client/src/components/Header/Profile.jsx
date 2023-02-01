e56e1daa387a1fcdbd3f2f9ab59b1e02ff05aea9
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-02 00:55:08 +0530

    logout handled

--- client/src/components/Header/Profile.jsx
+++ client/src/components/Header/Profile.jsx
@@ -1,19 +1,43 @@
-import React, { useContext } from 'react';
-import { Box, Typography } from '@mui/material';
+import React, { useContext, useState } from 'react';
+import { Box, Menu, MenuItem, Typography } from '@mui/material';
 import { DataContext } from '../../contextAPI/Dataprovider';
 
 import { capitalizeFirstLetter } from '../../constant/modify';
 
 
 const Profile = () => {
-	const { loggedinUser } = useContext(DataContext)
+	const { loggedinUser, setLoggedinUser } = useContext(DataContext);
+	const [menuOpen, setMenuOpen] = useState(false);
+
+	const clickHandler = (event)=>{
+		setMenuOpen(event.currentTarget);
+	}
+
+	const logoutHandler = ()=>{
+		setMenuOpen(false);
+		setLoggedinUser({});
+	}
+
 
 	return (
-		<Box>
-			<Typography style={{ marginTop: 3, marginRight: 40, fontWeight: 600, cursor: 'pointer', marginLeft: 20 }}>
-				{capitalizeFirstLetter(loggedinUser.name)}
-			</Typography>
-		</Box>
+		<>
+
+			<Box onClick={clickHandler}>
+				<Typography style={{ marginTop: 3, marginRight: 40, fontWeight: 600, cursor: 'pointer', marginLeft: 20 }}>
+					{capitalizeFirstLetter(loggedinUser.name)}
+				</Typography>
+			</Box>
+			<Menu
+				id="basic-menu"
+				anchorEl={menuOpen}
+				open={Boolean(menuOpen)}
+				onClose={logoutHandler}
+			>
+				<MenuItem >Profile</MenuItem>
+				<MenuItem >My account</MenuItem>
+				<MenuItem onClick={logoutHandler}>Logout</MenuItem>
+			</Menu>
+		</>
 	)
 }
 
