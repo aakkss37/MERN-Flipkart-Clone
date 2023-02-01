bdb4668cde640eea2a9f62f947b426e743db1234
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-01 17:05:30 +0530

    Login dialog box created input function ready

--- client/src/components/login/LoginDialog.jsx
+++ client/src/components/login/LoginDialog.jsx
@@ -42,6 +42,16 @@ const LoginButton = styled(Button)`
 		background: #FB641B;
 	}
 `
+const SignupButton = styled(Button)`
+    text-transform: none;
+    background: #FB641B;
+    color: #fff;
+    height: 48px;
+    border-radius: 2px;
+	&:hover{
+		background: #FB641B;
+	}
+`
 const RequestOTP = styled(Button)`
     text-transform: none;
     background: #fff;
@@ -62,59 +72,73 @@ const CreateAccount = styled(Typography)`
     font-size: 14px;
     cursor: pointer
 `
-
+const initialAccountDetail = {
+	name: '',
+	email: '',
+	password: '',
+	phone: '',
+}
 
 const LoginDialog = (props) => {
 	const [haveAccount, setHaveAccount] = useState(true)
+	const [accountDetail, setAccountDetail] = useState(initialAccountDetail)
 
-	const handleCloseDialogBox = ()=>{
+	const handleCloseDialogBox = () => {
 		props.setOpenDialog(false);
 		setHaveAccount(true);
 	}
+	const handleSignupUser = ()=>{
+		console.log(accountDetail);
+		setAccountDetail(initialAccountDetail);
+		setHaveAccount(true);
+	}
 
+	const inputChangeHandle = (e) => {
+		setAccountDetail({ ...accountDetail, [e.target.name]: e.target.value })
+	}
 
 	return (
-		<Dialog open={props.openDialog} onClose={ handleCloseDialogBox  } PaperProps={{ sx: { maxWidth: 'unset' } }}>
+		<Dialog open={props.openDialog} onClose={handleCloseDialogBox} PaperProps={{ sx: { maxWidth: 'unset' } }}>
 			{ /* PaperProps={{sx: {maxWidth: 'unset'}}}---> mui dialog box have a default max width... 
 			if your element takes occupied width more then that.. then a scrole bar will appear.. 
 			to prevent thant and remove the default max-width we need to set this property*/}
 			<Component>
 				{
 					haveAccount
-					?
-					<ImageBox>
-						<Typography variant="h5">Login</Typography>
-						<Typography style={{ marginTop: 20 }}>Get access to your <br />Orders,  Wishlist and Recommendations</Typography>
-					</ImageBox>
-					:
-					<ImageBox>
+						?
+						<ImageBox>
+							<Typography variant="h5">Login</Typography>
+							<Typography style={{ marginTop: 20 }}>Get access to your <br />Orders,  Wishlist and Recommendations</Typography>
+						</ImageBox>
+						:
+						<ImageBox>
 							<Typography variant="h5">Looks like you're new here!</Typography>
 							<Typography style={{ marginTop: 20 }}>Sign up with your mobile number to get started</Typography>
-					</ImageBox>
+						</ImageBox>
 				}
 				{
-					haveAccount 
-					?
-					<Wrapper>
-						<TextField id="standard-basic" label="Enter Email/mobile no." variant="standard" />
-						<TextField id="standard-basic" label="Enter Password" variant="standard" />
-						<Text >By continuing, you agree to Flipkart's Terms of Use and Privacy Policy.</Text>
-						<LoginButton>Login</LoginButton>
-						<Typography style={{ textAlign: "center" }}>OR</Typography>
-						<RequestOTP>Request OTP</RequestOTP>
-						<CreateAccount onClick={()=> setHaveAccount(false)} >New to Flipkart? Create an account</CreateAccount>
-					</Wrapper>
-					:
-					<Wrapper>
-						<TextField id="standard-basic" label="Enter Name" variant="standard" />
-						<TextField id="standard-basic" label="Enter Email" variant="standard" />
-						<TextField id="standard-basic" label="Enter Password" variant="standard" />
-						<TextField id="standard-basic" label="Enter Mobile no." variant="standard" />
-						<Text >By continuing, you agree to Flipkart's Terms of Use and Privacy Policy.</Text>
-						<LoginButton>Continue</LoginButton>
-						<Typography style={{ textAlign: "center" }}>OR</Typography>
-						<CreateAccount onClick={() => setHaveAccount(true)}>Already have account? Login</CreateAccount>
-					</Wrapper>
+					haveAccount
+						?
+						<Wrapper>
+							<TextField id="standard-basic" label="Enter Email/mobile no." variant="standard" />
+							<TextField id="standard-basic" label="Enter Password" type='password' variant="standard" />
+							<Text >By continuing, you agree to Flipkart's Terms of Use and Privacy Policy.</Text>
+							<LoginButton>Login</LoginButton>
+							<Typography style={{ textAlign: "center" }}>OR</Typography>
+							<RequestOTP>Request OTP</RequestOTP>
+							<CreateAccount onClick={() => setHaveAccount(false)} >New to Flipkart? Create an account</CreateAccount>
+						</Wrapper>
+						:
+						<Wrapper>
+							<TextField id="standard-basic" onChange={inputChangeHandle} name='name' value={accountDetail.name} label="Enter Name" variant="standard" />
+							<TextField id="standard-basic" onChange={inputChangeHandle} name='phone' value={accountDetail.phone} label="Enter Mobile no." variant="standard" />
+							<TextField id="standard-basic" onChange={inputChangeHandle} name='email' value={accountDetail.email} label="Enter Email" type='email' variant="standard" />
+							<TextField id="standard-basic" onChange={inputChangeHandle} name='password' value={accountDetail.password} label="Enter Password" type='password' variant="standard" />
+							<Text >By continuing, you agree to Flipkart's Terms of Use and Privacy Policy.</Text>
+							<SignupButton onClick={handleSignupUser}>Continue</SignupButton>
+							<Typography style={{ textAlign: "center" }}>OR</Typography>
+							<CreateAccount onClick={() => setHaveAccount(true)}>Already have account? Login</CreateAccount>
+						</Wrapper>
 				}
 
 			</Component>
