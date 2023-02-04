fa0c909507e17f0e488ae99b04ffd9298e7f0ed4
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-05 01:59:49 +0530

    404 error page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import DataProvider from "./contextAPI/Dataprovider";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DetailView from "./components/details/DetailView";
 import Cart from "./components/cart/Cart";
+import Error404 from "./components/Error404";
 
 
 
@@ -19,6 +20,7 @@ function App () {
 						<Route path='/' element={<Home />} />
 						<Route path='/product/detail' element={<DetailView />} />
 						<Route path='/cart' element={<Cart/>} />
+						<Route path="*" element={<Error404 />} />
 					</Routes>
 				</Box>
 			</BrowserRouter >
